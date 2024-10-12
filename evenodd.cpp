e438990532ab716829760157de5b5d06a531cc95
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-10-12 09:03:56 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the no: ";
+    cin>>n;
+    if(n%2==0)cout<<"even no";
+    if(n%2!=0)cout<<"odd no";
+    }
+
+
+
+
+
+
