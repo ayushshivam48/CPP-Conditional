d0019924a900d69c8202ee64deafb07aecbe998a
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-10-13 09:34:27 +0530

    Add files via upload

---
+++ threedigitnumber.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the no: ";
+    cin>>n;
+    if(n>99 && n<1000)cout<<"It's a three digit no";
+    else cout<<"It's not a three digit no";
+    }
