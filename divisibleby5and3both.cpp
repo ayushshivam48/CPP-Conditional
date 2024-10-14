7e60d822c13b34f7df66c56c86b19dd4c1d56fad
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-10-14 10:19:58 +0530

    Add files via upload

---
+++ divisibleby5and3both.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the no: ";
+    cin>>n;
+    if(n%5==0 && n%3==0)cout<<"It is divisible by both";
+    else cout<<"It is not divisible by both";
+    }
